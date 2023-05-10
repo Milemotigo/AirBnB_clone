cddad24725b8f68089cc1e472493dba91842bd03
Gibexz <andrewetedu@gmail.com>
2023-05-10 21:50:52 +0100

    Base_Models

--- models/base_model.py
+++ models/base_model.py
@@ -9,14 +9,16 @@ class base_model():
     """base model"""
     def __init__(self, id, created_at, updated_at):
         """constructor function"""
-        self.id = uuid.uuid4()
+        self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
-        return f"[<base_model>] (<self.id>) <self.__dict__>]"
+        """  """
+        return f"{[self.__class__.__name__]} {(self.id}) {self.__dict__}"
 
     def save(self):
+        """  """
         self.updated_at = datetime.now()
 
     def to_dict(self):
