dcd0e0aa2059e565c75f8397d41371ea3eacbe51
Gibexz <andrewetedu@gmail.com>
2023-05-11 12:26:11 +0100

    AirBnB Task 3

--- models/base_model.py
+++ models/base_model.py
@@ -7,22 +7,22 @@ from datetime import datetime
 
 class BaseModel():
     """base model"""
-    def __init__(self, id=0, created_at=0, updated_at=0):
+    def __init__(self, *args, **kwargs):
         """constructor function"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
-        """  """
+        """should print: [<class name>] (<self.id>) <self.__dict__>"""
         return f"{[self.__class__.__name__]} {(self.id)} {self.__dict__}"
 
     def save(self):
-        """  """
+        """ updates the public instance attribute updated_at with the current datetime"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """  """
+        """returns a dictionary containing all keys/values of __dict__ of the instance"""
         dictt = {}
         for key, val in self.__dict__.items():
             if key not in ["id", "created_at", "updated_at"]:
@@ -31,6 +31,5 @@ class BaseModel():
         dictt["updated_at"] = self.updated_at.isoformat()
         dictt["id"] = self.id
         dictt["created_at"] = self.created_at.isoformat()
-         
         return dictt
 
