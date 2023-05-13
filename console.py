b2fecb1891accfa040ce67554d0b20a38e6f5273
Gibexz <andrewetedu@gmail.com>
2023-05-13 17:15:24 +0100

    ArirBnB task6 and 7

--- console.py
+++ console.py
@@ -5,6 +5,7 @@
 '''
 import cmd
 from models.base_model import BaseModel
+import models
 
 class HBNBCommand(cmd.Cmd):
         prompt = '(hbnb)    '
@@ -28,16 +29,111 @@ class HBNBCommand(cmd.Cmd):
             """
             if not argss:
                 print("** class name missing **")
+                return
             else:
                 args = argss.split()
                 if len(args) != 1:
                     print("** class doesn't exist **")
+                    return
                 elif len(args) == 1 and args[0] != 'BaseModel':
                     print("** class doesn't exist **")
+                    return
                 else:
                     inst = BaseModel()
                     inst.save()
                     print(inst.id)
+
+        def do_show(self, argss):
+            """
+            Prints the string representation of an instance based on the class
+            name and id. Ex: $ show BaseModel 1234-1234-1234.
+
+                inst_rep: variable for string representation of an instance
+                object_store: __objects{} dict gotten from the FileStorage class
+            """
+            if not argss:
+                print("** class name missing **")
+            else:
+                args = argss.split()
+                if len(args) > 2:
+                    #print("fail")
+                    return
+                else:
+                    if len(args) == 1 and args[0] != "BaseModel":
+                        print("** class doesn't exist **")
+                        return
+                    if len(args) == 1 and args[0] == "BaseModel":
+                        print("** instance id missing **")
+                        return
+                    if len(args) == 2 and args[0] == "BaseModel":
+                        object_store = models.storage.all()
+                        key = args[0]+"."+args[1]
+                        inst_rep = object_store.get(key)
+                        if inst_rep:
+                            print(inst_rep)
+                        else:
+                            print("** no instance found **")
+
+        def do_destroy(self, argss):
+            """
+            Deletes an instance based on the class name and id (save the change
+            into the JSON file). Ex: $ destroy BaseModel 1234-1234-1234
+            """
+            if not argss:
+                print("** class name missing **")
+            else:
+                args = argss.split()
+                if len(args) > 2:
+                    #print("fail")
+                    return
+                else:
+                    if len(args) == 1 and args[0] != "BaseModel":
+                        print("** class doesn't exist **")
+                        return
+                    if len(args) == 1 and args[0] == "BaseModel":
+                        print("** instance id missing **")
+                        return
+                    if len(args) == 2 and args[0] == "BaseModel":
+                        object_store = models.storage.all()
+                        key = args[0]+"."+args[1]
+                        if key in object_store.keys():
+                            del object_store[key]
+                            models.storage.save()
+                        else:
+                            print("** no instance found **")
+                        
+        def do_all(self, argss):
+            """
+             Prints all string representation of all instances based or
+             not on the class name. Ex: $ all BaseModel or $ all
+            """
+            if not argss:
+                print("not on the class name")
+                return
+            else:
+                args = argss.split()
+                if len(args) > 1:
+                    #########
+                    return
+                else:
+                    if len(args) == 1 and args[0] != "BaseModel":
+                        print("** class doesn't exist **")
+                        return
+                    else:
+                        object_store = models.storage.all()
+                        #print(f"[{object_store}]"
+                        #print(str(object_store))
+                        item_str = []
+                        for value in object_store.values():
+                            val_str = f"{value}"
+                            item_str.append(val_str)
+
+                        result = ", ".join(item_str)
+                        print(f"[\"{result}\"]")
+
+
+
+
 
 
 if __name__ == '__main__':
